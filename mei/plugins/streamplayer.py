7fd8f14b7da736aae41a4e2c4e3454fa6a79d639
daxxar <daxxar@5ef17e78-b9bb-11dd-893d-d98698f66bbf>
2008-12-13 15:50:30 +0000

    Add vlc_argument config to streamplayer.

--- mei/plugins/streamplayer.py
+++ mei/plugins/streamplayer.py
@@ -4,9 +4,12 @@ from mei import plugin
 from mei.gui import util, vlc
 
 class StreamPlayer(plugin.Plugin, vlc.Vlc):
+    DEFAULT_CONFIG = {
+        'vlc_arguments': []
+    }
     def __init__(self, config, app):
         plugin.Plugin.__init__(self)
-        vlc.Vlc.__init__(self, app, '.', [config['stream'].encode()])
+        vlc.Vlc.__init__(self, app, '.', [config.get('stream').encode()] + config.get('vlc_arguments', []))
 
         self._app = app
         self._config = config
